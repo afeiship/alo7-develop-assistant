a09851b6a2f0ff53617aabe76e5d155b23374d2f
afeiship <1290657123@qq.com>
2020-10-15 18:20:30 +0800

    feat: 一键生成 relase notes

--- src/plugins/release-notes.js
+++ src/plugins/release-notes.js
@@ -4,20 +4,26 @@
  */
 
 $(document).ready(() => {
+  var author = $('.page-metadata .author').text();
+  if (!author.includes('Tinny Tao')) return;
+
   // console.log('rnotes ready!');
   var target = $('h1:contains("功能描述")');
-  target.css({ 'display': 'flex', 'align-items': 'center', 'justify-content': 'space-between' });
-  target.append('<button id="rnotes-copy" class="aui-button aui-button-primary">复制Release Notes</button>');
+  target.css({ display: 'flex', 'align-items': 'center', 'justify-content': 'space-between' });
+  target.append(
+    '<button id="rnotes-copy" class="aui-button aui-button-primary">Release Notes</button>'
+  );
 
   var tw = target.next('.table-wrap');
   var records = tw.find('.relative-table tbody tr');
   var texts = [];
-  records.each((idx, record)=>{
-    var text = $(record).find('td:eq(1)').text();
-    texts.push(`${idx + 1}. ${text}`);
-  })
 
-  $('#rnotes-copy').click(()=>{
+  records.each((idx, record) => {
+    var text = $(record).find('td:eq(1)').text();
+    text && texts.push(`${idx}. ${text}`);
+  });
+
+  $('#rnotes-copy').click(() => {
     gmsdk.setClipboard(texts.join('\n'));
     $.toast({
       icon: 'success',
