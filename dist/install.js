708038c3db1fdb782a8420cc47f72d890308d46f
afeiship <1290657123@qq.com>
2020-11-05 18:32:40 +0800

    fix: gm-sdk version

--- dist/install.js
+++ dist/install.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         alo7-develop-assistant
 // @namespace    https://github.com/afeiship/alo7-develop-assistant
-// @version      1.0.44
+// @version      1.0.45
 // @description  Develop assistant for alo7.
 // @author       aric.zheng
 // @include    *://*.saybot.net:*/*
@@ -13,8 +13,8 @@
 // @updateURL https://raw.githubusercontent.com/afeiship/alo7-develop-assistant/master/dist/install.js
 // @supportURL https://github.com/afeiship/alo7-develop-assistant/issues/new?assignees=adlered&labels=help+wanted&template=ISSUE_TEMPLATE.md&title=
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @require  https://cdn.jsdelivr.net/npm/@feizheng/gm-sdk@1.1.14
-// @require  https://cdn.jsdelivr.net/npm/@feizheng/alo7-develop-assistant@1.0.44
+// @require  https://cdn.jsdelivr.net/npm/@feizheng/gm-sdk@1.1.15
+// @require  https://cdn.jsdelivr.net/npm/@feizheng/alo7-develop-assistant@1.0.45
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js
 // @resource JQ_TOAST https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.css
 // @grant GM_addStyle
