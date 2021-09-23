18f526c74e6d59ac7e53106dd2043936fc1006c0
afeiship <1290657123@qq.com>
2021-09-23 17:55:20 +0800

    Release 1.0.71

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  *  name: @jswork/alo7-develop-assistant
  *  description: Develop assistant for alo7.
  *  homepage: https://github.com/afeiship
- *  version: 1.0.70
- *  date: 2021-09-23T09:50:25.311Z
+ *  version: 1.0.71
+ *  date: 2021-09-23T09:55:10.392Z
  *  license: MIT
  */
 
