4f187284fe931fee7bcb7f821f955b70890765ea
afeiship <1290657123@qq.com>
2022-06-27 18:41:26 +0800

    refactor: upt dev_url and input data-action

--- src/plugins/bio-iframe-dev.js
+++ src/plugins/bio-iframe-dev.js
@@ -9,7 +9,19 @@ $(document).ready(() => {
   var docUrl = document.URL;
   if (!docUrl.includes('bio-platform-frontend.beta')) return false;
 
+  const DEV_URL = '__dev_url__';
+
   var App = nx.declare({
+    properties: {
+      devUrl: {
+        set: function (val) {
+          localStorage.setItem(DEV_URL, val);
+        },
+        get: function () {
+          return localStorage.getItem(DEV_URL);
+        }
+      }
+    },
     methods: {
       start() {
         this.iniEvents();
@@ -23,40 +35,38 @@ $(document).ready(() => {
           self.inject();
         });
 
-        $('body').on('input', '#dev-input', function (inEvent) {
-          localStorage.setItem('__dev_url__', inEvent.target.value);
+        $('body').on('input', '[data-action="input"]', function (inEvent) {
+          self.devUrl = inEvent.target.value;
         });
 
         $('body').on('click', '[data-action="reset"]', function (inEvent) {
           console.log('click reset.', inEvent);
-          localStorage.removeItem('__dev_url__');
+          localStorage.removeItem(DEV_URL);
           window.location.reload();
         });
       },
       initElements() {
-        const devUrl = localStorage.getItem('__dev_url__');
         $('[class^="Header___StyledHeader"]').prepend(`
           <form class="gm-row gm-row-center" data-role="form">
-            <input type="text" id="dev-input" value="${devUrl}" class="gm-form-control" placeholder="请输入你的开发URL地址">
+            <input type="text" data-action="input" value="${this.devUrl}" class="gm-form-control" placeholder="请输入你的开发URL地址">
             <button style="margin-left: 10px" class="gm-btn gm-btn-primary is-large" data-action="inject">注入URL</button>
             <button type="button" style="margin-left: 10px" class="gm-btn gm-btn-default is-large" data-action="reset">重置</button>
           </form>
         `);
       },
       syncIframe() {
-        const devUrl = localStorage.getItem('__dev_url__');
-        if (devUrl) document.querySelector('iframe').src = devUrl;
+        if (this.devUrl) document.querySelector('iframe').src = this.devUrl;
       },
       inject() {
-        const el = document.getElementById('dev-input');
-        localStorage.setItem('__dev_url__', el.value);
+        const el = document.querySelector('[data-action="input"]');
+        this.devUrl = el.value;
         this.syncIframe();
         window.location.reload();
       }
     }
   });
 
-  nx.waitToDisplay('iframe', 1000, (el) => {
+  nx.waitToDisplay('iframe', 1000, () => {
     var app = new App();
     app.start();
   });
