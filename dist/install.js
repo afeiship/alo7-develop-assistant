f661925da89984fb88e0de4de5830a361b64d80e
afeiship <1290657123@qq.com>
2022-07-27 08:56:24 +0800

    Release 1.0.82

--- dist/install.js
+++ dist/install.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         alo7-develop-assistant
 // @namespace    https://github.com/afeiship/alo7-develop-assistant
-// @version      1.0.81
+// @version      1.0.82
 // @description  Develop assistant for alo7.
 // @author       aric.zheng
 // @include    *://*.saybot.net:*/*
 // @include    *://*.saybot.net/*
+// @include    *://*.aigene.org.cn/*
 // @include    *://*.saybot.com/*
 // @include    *://*.shuobaotang.com/*
 // @include    *://*.alo7.com/*
@@ -14,7 +15,7 @@
 // @supportURL https://github.com/afeiship/alo7-develop-assistant/issues/new?assignees=adlered&labels=help+wanted&template=ISSUE_TEMPLATE.md&title=
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require  https://unpkg.com/@jswork/gm-sdk@1.0.21
-// @require  https://cdn.jsdelivr.net/npm/@jswork/alo7-develop-assistant@1.0.81
+// @require  https://cdn.jsdelivr.net/npm/@jswork/alo7-develop-assistant@1.0.82
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js
 // @resource JQ_TOAST https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.css
